eab2b04b3d271348c9d71905a954fb27fd1bef3c
RyanLiu <ruoyanlau@gmail.com>
2024-05-07 23:41:12 +0800

    [add] lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,22 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author RyanL
  */
 public class LeapYear {
+    /** Determines whether a year is a leapyear.
+     * @param year
+     * @return boolean
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
